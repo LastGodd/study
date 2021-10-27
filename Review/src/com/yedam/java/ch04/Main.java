fb615930006bf8d7aac219175e33866226732e15
admin <admin@DESKTOP-0OK8IRJ>
2021-10-27 10:12:41 +0900

    211026

---
+++ Review/src/com/yedam/java/ch04/Main.java
@@ -0,0 +1,14 @@
+package com.yedam.java.ch04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//문제4) 로또 번호를 구하는 프로그램을 작성하세요.
+		
+		
+			
+	}
+
+}
