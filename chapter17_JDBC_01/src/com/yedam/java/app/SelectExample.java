664614277fe5370a9aa935dfd8ac3df28d4f281c
admin <admin@DESKTOP-0OK8IRJ>
2021-11-05 17:43:32 +0900

    211105_pm

--- chapter17_JDBC_01/src/com/yedam/java/app/SelectExample.java
+++ chapter17_JDBC_01/src/com/yedam/java/app/SelectExample.java
@@ -17,19 +17,21 @@ public class SelectExample {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String id = "hr";
 		String password = "hr";
+		
 		Connection conn = DriverManager.getConnection(url, id, password);
 
-		// 3. Statement or PreparedStatement 객체 생성하기
+		// 3. Statement or PreparedStatement 객체 생성하기 (운반 역할)
 		Statement stmt = conn.createStatement();
 
 		// 4. SQL 실행
+		// executeUpdate 는 인설트한 갯수 반환
 		ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
-
+	
 		// 5. 결과값 받아서 출력하기
 		while (rs.next()) {
-			String name = "이름 : " + rs.getString("First_name");
+			String name = "이름 : " + rs.getString("first_name");
 			System.out.println(name);
-
+			
 		}
 
 		// 6. 자원을 해제 하기 (실행의 역순으로 해제하기)
